2bcc50d6ed482c575198bc0e6cef9d5490e5284a
sirdir <phddir@gmail.com>
2017-06-08 22:31:59 +0300

    redone to groovy + spock tests

--- src/test/groovy/api/TwitterTest.groovy
+++ src/test/groovy/api/TwitterTest.groovy
@@ -1,20 +1,19 @@
 package api
 
+import com.sun.xml.internal.bind.v2.TODO
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 import twitter4j.Status
 import twitter4j.TwitterException
 import twitter4j.TwitterFactory
 import twitter4j.conf.ConfigurationBuilder
 
-import java.time.LocalDateTime
-import java.time.ZoneId
-
 class TwitterTest extends Specification {
 
     @Shared twitter
 
-    def setup() {
+    def setupSpec() {
         def cb = new ConfigurationBuilder()
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("gzY5BWKxHN5SfBhDgZ5Q28Aqm")
@@ -26,41 +25,55 @@ class TwitterTest extends Specification {
         assert twitter
     }
 
+    @Unroll
     def "Time Line parse"() {
-        List<Status> statuses = twitter.getHomeTimeline()
-        for (Status st : statuses) {
-            LocalDateTime lcdt = LocalDateTime.ofInstant(st.getCreatedAt().toInstant(), ZoneId.systemDefault())
-            System.out.println("----------")
-            System.out.println(lcdt)
-            System.out.println(st.getRetweetCount())
-            System.out.println(st.getText())
-            System.out.println("----------")
-        }
+        expect:
+        def date = new Date().parse('EEE MMM dd HH:mm:ss zzzz yyyy', dateStr)
+        st.getCreatedAt() == date
+        st.getRetweetCount() == retwit
+        st.getText() == text
+
+        where:
+        st << twitter.getHomeTimeline()
+        dateStr << ['Wed Jun 07 20:48:47 EEST 2017', 'Wed Jun 07 20:48:02 EEST 2017']
+        retwit << [0, 0]
+        text << ['E = mc^2', 'Привет, Твиттер! #мойпервыйТвит']
     }
 
     def "Update status"(){
+        setup:
         def textOfTwit = 'something'
+        def errCode = 403
+        def errMsg = 'Status is a duplicate.'
 
+        when:
         Status status = twitter.updateStatus(textOfTwit)
-        System.out.println("Successfully updated the status to [" + status.getText() + "].")
 
-        try {
-            twitter.updateStatus(textOfTwit)
-            assert false
-        }
-        catch (TwitterException e){
-            assert e.getStatusCode() == 403
-            assert e.getErrorMessage() == 'Status is a duplicate.'
-        }
+        then:
+        status
+
+        when:
+        twitter.updateStatus(textOfTwit)
+
+        then:
+        def e = thrown(TwitterException)
+        e.getStatusCode() == errCode
+        e.getErrorMessage() == errMsg
+
+//        cleanup: TODO
     }
 
     def "Destroy twit"() {
+        setup:
         def textOfTwit = 'to delete'
 
+        when:
         def status = twitter.updateStatus(textOfTwit)
-
         status = twitter.destroyStatus(status.getId())
 
-        assert status.getText() == textOfTwit
+        then:
+        status.getText() == textOfTwit
+
+//        cleanup: TODO
     }
 }

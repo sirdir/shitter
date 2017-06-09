2349f2679f93c86489bbec0fad194c1a3d294e39
sirdir <phddir@gmail.com>
2017-06-09 03:10:06 +0300

    basic us tests for gebish

--- src/test/groovy/ui/TwitterTest.groovy
+++ src/test/groovy/ui/TwitterTest.groovy
@@ -1,19 +1,42 @@
 package ui
 
-import spock.lang.Specification
+import geb.Browser
+import geb.driver.CachingDriverFactory
+import org.openqa.selenium.chrome.ChromeDriver
+import spock.lang.*
+
 
 class TwitterTest extends Specification{
 
-    def setupSpec() {
+    Browser browser
 
+    def setup() {
+        browser = new Browser(driver: new ChromeDriver())
+        browser.setBaseUrl('http://www.gebish.org/')
+    }
+
+    def cleanup() {
+        CachingDriverFactory.clearCache()
+        browser.quit()
     }
 
     def "Time Line parse"() {
+        when:
+        browser.go()
+
+        then:
+        browser.getTitle() == "Geb - Very Groovy Browser Automation"
     }
 
     def "twit something"(){
+        when:
+        browser.go('/manual/current/')
+
+        then:
+        browser.$('#driver-quirks').text() == '3.3. Driver quirks'
    }
 
     def "delete twit"() {
+
     }
 }
